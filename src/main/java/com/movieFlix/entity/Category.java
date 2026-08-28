b4cc85acd9b7cd3905b28fa735f1e2bfcb62ecd7
Guilherme Sousa Borges <guilhermepllay157@gmail.com>
2026-08-27 21:16:26 -0300

    fix(category): added contructors lombok annotation

--- src/main/java/com/movieFlix/entity/Category.java
+++ src/main/java/com/movieFlix/entity/Category.java
@@ -1,14 +1,14 @@
 package com.movieFlix.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "category")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Category {
 
